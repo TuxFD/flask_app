c149265744b4e9c977740af82f710ffb396374ce
TuxFD <tuxfd@yandex.ru>
2024-09-04 17:19:13 +0300

    v2_template

--- app.py
+++ app.py
@@ -14,9 +14,17 @@ App:
 """
 
 from flask import Flask
+from flask import render_template
 
 app = Flask("Flask project")
 
+# ===============================================================
+
+@app.route("/")
+def page():
+    return render_template("index.html")
+
+# ===============================================================
 
 if __name__ == "__main__":
     app.run(debug=True)
